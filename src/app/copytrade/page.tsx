7099c5c193d3e77709a83e888d5236dbdbb06ba0
tonipcv <xppsalvador@gmail.com>
2025-06-13 11:53:31 -0300

    abertura

---
+++ src/app/copytrade/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+export default function Info() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-gray-900 via-gray-800 to-black flex flex-col items-center justify-center p-4">
+      <div className="w-full max-w-3xl mx-auto">
+        <iframe 
+          src="https://scripts.converteai.net/32ff2495-c71e-49ba-811b-00b5b49c517f/players/684c333dae3ffc76ce6b719e/embed.html"
+          width="100%"
+          height="500"
+          frameBorder="0"
+          allowFullScreen
+        />
+      </div>
+    </div>
+  );
+} 
